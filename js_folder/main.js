5fce4dff1534f2e7fdb107e0cd37fda16efbb722
pinkxpink00 <kyperu07@gmail.com>
2023-09-08 02:26:54 +0300

    data type

--- js_folder/main.js
+++ js_folder/main.js
@@ -4,5 +4,17 @@ let someVariable;
 username = "Tom";
 someVariable = 50;
 
-console.log(username);
-console.log(someVariable);
+console.log`Before:${username}`;
+console.log("Before:",someVariable);
+
+username = "Bob";
+someVariable = 10.4;
+
+console.log("after:",username);
+console.log("after:",someVariable);
+
+let email;
+console.log(typeof email,email);
+
+const user = {name: "German", age: 24};
+console.log(typeof user,user);
