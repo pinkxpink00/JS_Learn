4cfcf6ad69b44c39658010865dfe5d3e759a69d3
pinkxpink00 <kyperu07@gmail.com>
2023-09-13 21:56:37 +0300

    func v2

--- practice.js
+++ practice.js
@@ -1,18 +1,17 @@
-var i,base,power,result;
+var i,out;
 
-base = 2; power = 2;
-pow();
-console.log(result);
+out = pow(2,2);
+console.log(out);
 
-base = 4; power = 5;
-pow();
-console.log(result);
+out = pow(5,5);
+console.log(out);
 
-function pow()
+function pow(base,power)
 {
     result = 1;
     for (i=0;i<power;i++)
     {
         result *= base;
     }
+    return result;
 }
