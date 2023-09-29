a88c41cc8533c9f219a27f2b985716795c7aa0dd
pinkxpink00 <kyperu07@gmail.com>
2023-09-30 00:09:24 +0300

    Array learned

--- practice.js
+++ practice.js
@@ -4,3 +4,18 @@ const num2 = 3;
 const res = Number(num1) + num2;
 
 console.log(res);
+
+
+
+const num3 = 1;
+const num4 = "4";
+
+const res1 = num3 + parseInt(num4);
+
+console.log(res1);
+
+const myArray = ["Tom","Alice","Bob"];
+console.log(myArray);
+console.log(myArray[1],myArray[0],myArray[2])
+const digitArray = myArray[5] = 3;
+console.log(digitArray);
